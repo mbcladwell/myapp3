53f7a064d39303c16a549b554654eebd558990e1
mbcladwell <mbcladwell@gmail.com>
2022-11-12 03:40:41 -0500

    autocommit

--- myapp3/guix.scm
+++ myapp3/guix.scm
@@ -1,5 +1,4 @@
 (define-module (myapp3)
-  #:use-module ((guix licenses) #:prefix license:)
   #:use-module (gnu packages)
   #:use-module (guix packages)
   #:use-module (guix download)
@@ -10,6 +9,13 @@
   #:use-module (guix utils)
   #:use-module ((guix build utils) #:select (alist-replace))
   #:use-module (ice-9 match)
+;;  #:use-module (gnu home)
+;;  #:use-module (gnu home services)
+;;  #:use-module (gnu home services shells)
+;;  #:use-module (gnu services)
+;;  #:use-module (gnu packages admin)
+  #:use-module (guix gexp)
+  #:use-module ((guix licenses) #:prefix license:)
   #:use-module ((srfi srfi-1) #:select (alist-delete)))
 
 
@@ -22,9 +28,9 @@
        (method git-fetch)
        (uri (git-reference
              (url "https://github.com/mbcladwell/myapp3.git")
-             (commit "17235bab2264bf24a246982f6ad2ad9d1133cd7e")))
+             (commit "f2610b50d452f03f1c4745c8cefb5c59c4a38b45")))
        (sha256
-        (base32 "02vhyy5rajww0n0ggkry4jkr1gyqnx4kb9fb03hbdbfw86zsvxn0"))))
+             (base32 "03yvjbw1nhsjd99xq5xl4kgb4xjkr0abmqcjb3a0h56kq23kxd66"))))
     (build-system guile-build-system)
     (native-inputs
      `(("guile" ,guile-3.0)))
