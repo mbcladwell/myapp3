21adccd661ae704f5986ba4d9713d2b5e9c66ddb
mbcladwell <mbcladwell@gmail.com>
2022-11-14 06:14:18 -0500

    test

--- guix.scm
+++ guix.scm
@@ -1,5 +1,5 @@
 (define-module (myapp3)
-  #:use-module (gnu packages)
+ #:use-module (gnu packages)
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix git-download)
@@ -14,25 +14,42 @@
   #:use-module ((srfi srfi-1) #:select (alist-delete)))
 
 (define-public myapp3
-  (package
-    (name "myapp3")
-    (version "0.1")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/mbcladwell/myapp3.git")
-             (commit "6616c7e85cb2906afc580c2ba14fb1c44d204b75")))
-       (sha256
-        (base32 "09ir68fx9ymgigffszs21h2r6f5n9h5fahzag1p2sn3yl0a2m98x")) ))
-      
-    (build-system guile-build-system)
-    (native-inputs
-     `(("guile" ,guile-3.0)))
-    (home-page "https://github.com/mbcladwell/myapp3")
-    (synopsis "Colorized REPL for Guile")
-    (description
-     "Guile-colorized provides you with a colorized REPL for GNU Guile.")
-    (license license:gpl3+)))
+  (let ((commit "5be1009ec3dbb3cd28e0480768b9750d19b5d1b3")
+        (revision "1"))
+    (package
+      (name "myapp3")
+      (version (string-append "0-" revision "." (string-take commit 7)))
+      (source (origin
+                (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/mbcladwell/myapp3.git")
+                      (commit commit)))
+                (file-name (git-file-name name version))
+                (sha256
+                 (base32
+                  "0vlgld7xyh30n5pyp1hhavnjwy0551byxswj70b4aaifbc3dqh9p"))))
+      (build-system guile-build-system)
+      ;; (arguments
+      ;;  '(#:phases
+      ;;    (modify-phases %standard-phases
+      ;;      (add-after 'unpack 'patch
+      ;;        (lambda* (#:key inputs #:allow-other-keys)
+      ;;          (substitute* "squee.scm"
+      ;;            (("dynamic-link \"libpq\"")
+      ;;             (string-append
+      ;;              "dynamic-link \""
+      ;;              (assoc-ref inputs "postgresql") "/lib/libpq.so"
+      ;;              "\"")))
+      ;;          #t)))))
+;      (inputs
+ ;      `(("postgresql" ,postgresql)))
+      (native-inputs
+       `(("guile" ,guile-3.0)))
+      (home-page "https://notabug.org/cwebber/guile-squee")
+      (synopsis "Connect to PostgreSQL using Guile")
+      (description
+       "@code{squee} is a Guile library for connecting to PostgreSQL databases
+using Guile's foreign function interface.")
+      (license license:lgpl3+))))
 
 myapp3
