dd1386213557b81e63d0e5c321270b96c9ffc1ee
mbcladwell <mbcladwell@gmail.com>
2025-04-12 07:47:44 -0400

    autocommit

--- scripts/myapp3.sh
+++ scripts/myapp3.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 export LC_ALL="C"
-guileexecutable -e '(myapp3)' -s conmanstorepath/share/guile/site/3.0/myapp3.scm $1
+guileexecutable -e '(myapp3)' -s myapp3storepath/share/guile/site/3.0/myapp3.scm $1
 
